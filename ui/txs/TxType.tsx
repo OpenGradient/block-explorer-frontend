5eb63bdc4fb62c543205aebcf648885b4eec3e96
balogh.adam@icloud.com <adambalogh@Adams-MacBook-Pro.local>
2026-01-10 23:18:36 +0000

    color

--- ui/txs/TxType.tsx
+++ ui/txs/TxType.tsx
@@ -42,15 +42,15 @@ const TxType = ({ types, isLoading }: Props) => {
       break;
     case 'token_transfer':
       label = 'Transfer';
-      colorPalette = 'orange';
+      colorPalette = 'teal';
       break;
     case 'token_creation':
       label = 'Token';
-      colorPalette = 'orange';
+      colorPalette = 'teal';
       break;
     case 'coin_transfer':
       label = 'Transfer';
-      colorPalette = 'orange';
+      colorPalette = 'teal';
       break;
     case 'rootstock_remasc':
       label = 'REMASC';
